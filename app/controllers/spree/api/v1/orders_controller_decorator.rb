f51ed5f31d0b58d368559769f8db6ca8c1b51935
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-05-26 13:02:06 +0530

    Fixed orders listing api

--- app/controllers/spree/api/v1/orders_controller_decorator.rb
+++ app/controllers/spree/api/v1/orders_controller_decorator.rb
@@ -2,18 +2,21 @@ module Spree
 	Api::V1::OrdersController.class_eval do 
 
 		def index
-			@orders_list = {}
+			@orders_list = []
 			@stores = Merchant::Store.all
 			unless @stores.blank?
 				@stores.each do |store|
-					unless store.store_orders.blank?
-						@orders_list = store.store_orders.map do |s_o|
+					unless store.pickable_store_orders.blank?
+						@orders_list << store.pickable_store_orders.map do |s_o|
 							{number: s_o.number, store_name: store.name}
 						end
 					end
 				end
 			end
-			render json: @orders_list.to_json
+		rescue Exception => e
+			api_exception_handler(e)
+		ensure
+			render json: @orders_list
 		end
 
 	end
