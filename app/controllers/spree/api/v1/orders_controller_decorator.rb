8624eb4743e62d75ce0650d584ab46f1d137e0b2
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-05-26 12:16:40 +0530

    Fixed issues with missing parameters

--- app/controllers/spree/api/v1/orders_controller_decorator.rb
+++ app/controllers/spree/api/v1/orders_controller_decorator.rb
@@ -2,16 +2,18 @@ module Spree
 	Api::V1::OrdersController.class_eval do 
 
 		def index
-			@orders = []
+			@orders_list = {}
 			@stores = Merchant::Store.all
 			unless @stores.blank?
 				@stores.each do |store|
-					@orders << store.store_orders
+					unless store.store_orders.blank?
+						@orders_list = store.store_orders.map do |s_o|
+							{number: s_o.number, store_name: store.name}
+						end
+					end
 				end
 			end
-			render json: @orders.flatten.to_josn({
-				only: [:number]
-			})
+			render json: @orders_list.to_json
 		end
 
 	end
