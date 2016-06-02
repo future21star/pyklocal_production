06683f5bba4e37867fe0d1d2f8b37b8bb6a731a7
Gopal Sharma <gopal.sharma@w3villa.com>
2016-06-02 14:08:50 +0530

    bug fixed in header and shop page

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -1,14 +1,14 @@
 class Spree::ShopController < Spree::StoreController
 
-	def index		
-		@products = Spree::Product.all
-		@taxons = Spree::Taxon.where.not(name:"categories") 
-		@taxonomies = Spree::Taxonomy.includes(root: :children) 
-	end
+  def index   
+    @products = Spree::Product.all
+    @taxons = Spree::Taxon.where.not(name:"categories") 
+    @taxonomies = Spree::Taxonomy.includes(root: :children) 
+  end
 
-	def show 
-		@taxons = Spree::Taxon.where.not(name:"categories")
-		@products = Spree::Taxon.where(name: params[:id]).first.products   	
-	end
+  def show 
+    @taxons = Spree::Taxon.where.not(name:"categories")
+    @products = Spree::Taxon.where(name: params[:id]).first.try(:products)    
+  end
 
 end
