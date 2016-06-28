5abeafe8d183a2003adfd902144a52719bcbf568
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-06-28 17:33:50 +0530

    changed in shop controller

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -3,7 +3,7 @@ class Spree::ShopController < Spree::StoreController
   def index 
     @price_array = params[:q][:price].to_s if params[:q] && params[:q][:price]
     @all_facets = Sunspot.search(Spree::Product) do 
-      with(:location).in_radius(params[:lat], params[:lng], params[:radius].to_i, bbox: true) if params[:lat].present? && params[:lng].present?
+      with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
       facet(:price, :range => Spree::Product.min_price..Spree::Product.max_price, :range_interval => 100)
     end
     @search = Sunspot.search(Spree::Product) do 
