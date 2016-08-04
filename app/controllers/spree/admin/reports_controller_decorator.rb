6ed10451718eaafc522b97e94f1b5be77367de16
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-08-04 17:00:35 +0530

    Implemented sorting for reports

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -10,8 +10,24 @@ module Spree
 
       def products_sale_report
         params[:q] = {} unless params[:q]
-        @search = Spree::Product.joins(:orders).ransack(params[:q])
-        @products = @search.result
+        if params[:q][:orders_completed_at_gt].blank?
+          params[:q][:orders_completed_at_gt] = Time.zone.now.beginning_of_month
+        else
+          params[:q][:orders_completed_at_gt] = Time.zone.parse(params[:q][:orders_completed_at_gt]).beginning_of_day rescue Time.zone.now.beginning_of_month
+        end
+
+        if params[:q] && !params[:q][:orders_completed_at_lt].blank?
+          params[:q][:orders_completed_at_lt] = Time.zone.parse(params[:q][:orders_completed_at_lt]).end_of_day rescue ""
+        end
+
+        if params[:q] && !params[:q][:sell_count_gt].blank?
+          params[:q][:sell_count_gt] = "sell_count desc"
+        end
+
+        params[:q][:s] ||= "orders_completed_at desc"
+        
+        @search = Spree::Product.ransack(params[:q])
+        @products = @search.result(distinct: true).joins(:orders)
         @merchant = Merchant::Store.all
       end
 
