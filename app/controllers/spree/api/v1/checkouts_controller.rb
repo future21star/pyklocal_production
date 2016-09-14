879d15376207310b49c865784d62fb82b32fc329
Manish rai <manish.rai@w3villa.com>
2016-09-14 18:52:49 +0530

    Convert the json response into String format of login ,logout, registration

---
+++ app/controllers/spree/api/v1/checkouts_controller.rb
@@ -0,0 +1,53 @@
+Spree::CheckoutsController.class_eval do
+	before_action :load_order_with_lock
+
+	 include Spree::Core::ControllerHelpers::Auth
+   include Spree::Core::ControllerHelpers::Order
+   # This before_action comes from Spree::Core::ControllerHelpers::Order
+   skip_before_action :set_current_order
+    def next
+      authorize! :update, @order, order_token
+      @order.next!
+      render json: {
+      	order_detail: (@order, [])}
+    rescue StateMachines::InvalidTransition
+      respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
+    end
+
+    private 
+
+   	def to_stringify_checkout_json obj ,values = []
+   		order_hash=Hash.new
+   		skip_order_attributes = ["last_ip_address","created_by_id","approver_id","approved_at","confirmation_delivered","guest_token","canceled_at","store_id"]
+   		obj.each do |c_obj|
+   			c_obj.attributes.each do |k,v|
+   					unless skip_order_attributes.include? k
+   						if k.eq?"bill_address_id"  
+   							bill_address_hash = Hash.new
+   							if v.present?
+   								Spree::Address.find(v).attributes.each do |k1,v1|
+   									bill_address_hash[k1.to_sym] = v1.to_s
+   								end
+   								order_hash["bill_address"] = bill_address_hash
+   							else
+   								order_hash["bill_address"] = ""
+   							end
+   						end
+
+   						if k.eq?"ship_address_id"  
+   							ship_address_hash = Hash.new
+   							if v.present?
+   								Spree::Address.find(v).attributes.each do |k1,v1|
+   									bill_address_hash[k1.to_sym] = v1.to_s
+   								end
+   								order_hash["bill_address"] = bill_address_hash
+   							else
+   								order_hash["ship_address"] = ""
+   							end
+   						end
+   					end 
+   			end
+   		end
+   		values.push(main_hash)
+   	end
+end
--- app/controllers/spree/api/v1/password_controller.rb
+++ app/controllers/spree/api/v1/password_controller.rb
@@ -4,10 +4,14 @@ module Spree
 		def create
 			user = Spree::User.find_by_email(params[:email])
 			unless user.blank?
-				user.send_reset_password_instructions
-				render json: {code: 1, message: "Link send successfully"}
+				begin
+					user.send_reset_password_instructions
+					render json: {code: "1", message: "Link send successfully"}
+				rescue Exception => e
+					render json: { code: "0" ,message: "Something went wrong" , Error: e.message }
+				end
 			else
-				render json: {code: 0, message: "Invalid email"}
+				render json: {code: "0", message: "Invalid email"}
 			end
 		end
 	end
