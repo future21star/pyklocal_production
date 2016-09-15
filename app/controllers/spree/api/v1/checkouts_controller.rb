842cb17468db018ea34f8f4d4fc0085ccfc6f1dc
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-09-15 12:39:42 +0530

    Fixed issues

--- app/controllers/spree/api/v1/checkouts_controller.rb
+++ app/controllers/spree/api/v1/checkouts_controller.rb
@@ -17,38 +17,38 @@ Spree::CheckoutsController.class_eval do
 
   private 
 
- 	def to_stringify_checkout_json obj ,values = []
- 		order_hash=Hash.new
- 		skip_order_attributes = ["last_ip_address","created_by_id","approver_id","approved_at","confirmation_delivered","guest_token","canceled_at","store_id"]
- 		obj.each do |c_obj|
- 			c_obj.attributes.each do |k,v|
- 					unless skip_order_attributes.include? k
- 						if k.eq?"bill_address_id"  
- 							bill_address_hash = Hash.new
- 							if v.present?
- 								Spree::Address.find(v).attributes.each do |k1,v1|
- 									bill_address_hash[k1.to_sym] = v1.to_s
- 								end
- 								order_hash["bill_address"] = bill_address_hash
- 							else
- 								order_hash["bill_address"] = ""
- 							end
- 						end
+ 	# def to_stringify_checkout_json obj ,values = []
+ 	# 	order_hash=Hash.new
+ 	# 	skip_order_attributes = ["last_ip_address","created_by_id","approver_id","approved_at","confirmation_delivered","guest_token","canceled_at","store_id"]
+ 	# 	obj.each do |c_obj|
+ 	# 		c_obj.attributes.each do |k,v|
+ 	# 				unless skip_order_attributes.include? k
+ 	# 					if k.eq?"bill_address_id"  
+ 	# 						bill_address_hash = Hash.new
+ 	# 						if v.present?
+ 	# 							Spree::Address.find(v).attributes.each do |k1,v1|
+ 	# 								bill_address_hash[k1.to_sym] = v1.to_s
+ 	# 							end
+ 	# 							order_hash["bill_address"] = bill_address_hash
+ 	# 						else
+ 	# 							order_hash["bill_address"] = ""
+ 	# 						end
+ 	# 					end
 
- 						if k.eq?"ship_address_id"  
- 							ship_address_hash = Hash.new
- 							if v.present?
- 								Spree::Address.find(v).attributes.each do |k1,v1|
- 									bill_address_hash[k1.to_sym] = v1.to_s
- 								end
- 								order_hash["bill_address"] = bill_address_hash
- 							else
- 								order_hash["ship_address"] = ""
- 							end
- 						end
- 					end 
- 			end
- 		end
- 		values.push(main_hash)
- 	end
+ 	# 					if k.eq?"ship_address_id"  
+ 	# 						ship_address_hash = Hash.new
+ 	# 						if v.present?
+ 	# 							Spree::Address.find(v).attributes.each do |k1,v1|
+ 	# 								bill_address_hash[k1.to_sym] = v1.to_s
+ 	# 							end
+ 	# 							order_hash["bill_address"] = bill_address_hash
+ 	# 						else
+ 	# 							order_hash["ship_address"] = ""
+ 	# 						end
+ 	# 					end
+ 	# 				end 
+ 	# 		end
+ 	# 	end
+ 	# 	values.push(main_hash)
+ 	# end
 end
