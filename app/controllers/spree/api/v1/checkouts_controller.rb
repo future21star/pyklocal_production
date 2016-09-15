a0c48c3c46aeb633caec54563e23e7e777160f03
Manish rai <manish.rai@w3villa.com>
2016-09-15 12:33:13 +0530

    status tag consistent in all apis

--- app/controllers/spree/api/v1/checkouts_controller.rb
+++ app/controllers/spree/api/v1/checkouts_controller.rb
@@ -9,7 +9,8 @@ Spree::CheckoutsController.class_eval do
       authorize! :update, @order, order_token
       @order.next!
       render json: {
-      	order_detail: (@order, [])}
+      	order_detail: to_stringify_checkout_json(@order, [])
+      }
     rescue StateMachines::InvalidTransition
       respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
     end
@@ -23,8 +24,8 @@ Spree::CheckoutsController.class_eval do
    			c_obj.attributes.each do |k,v|
    					unless skip_order_attributes.include? k
    						if k.eq?"bill_address_id"  
-   							bill_address_hash = Hash.new
    							if v.present?
+   								bill_address_hash = Hash.new
    								Spree::Address.find(v).attributes.each do |k1,v1|
    									bill_address_hash[k1.to_sym] = v1.to_s
    								end
@@ -35,8 +36,8 @@ Spree::CheckoutsController.class_eval do
    						end
 
    						if k.eq?"ship_address_id"  
-   							ship_address_hash = Hash.new
    							if v.present?
+   								ship_address_hash = Hash.new
    								Spree::Address.find(v).attributes.each do |k1,v1|
    									bill_address_hash[k1.to_sym] = v1.to_s
    								end
@@ -47,6 +48,22 @@ Spree::CheckoutsController.class_eval do
    						end
    					end 
    			end
+
+   			if c_obj.try(:line_items
+   				line_items_arr = []
+   				c_obj.line_items.each do|line_item|
+   					line_items_hash = Hash.new
+   					line_items_hash["id".to_sym] = line_item.id.to_s
+   					line_items_hash["quantity".to_sym] = line_item.quantity.to_s
+   					line_items_hash["price".to_sym] = line_item.price.to_s
+   					line_items_hash["variant_id".to_sym] = line_item.variant_id.to_s
+   					line_item.variant.product
+
+   				end
+   			else
+   				order_hash["line_items"] = []
+   			end
+
    		end
    		values.push(main_hash)
    	end
