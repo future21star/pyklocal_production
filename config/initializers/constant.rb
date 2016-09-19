705b0d86a8efb27047e34dfa974a2c2efc0a02d5
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-09-19 16:22:29 +0530

    Make delivery type updatable and show which item is pickup and which one is for home delivery after placing order

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,3 +1,3 @@
-DELIVERY_TYPE = {home_delivery: 'Home Delivery', take_away: 'Take Away'}
+DELIVERY_TYPE = {home_delivery: 'Home Delivery', pickup: 'Pickup'}
 REDIS_CLIENT = Redis.new(YAML.load_file(Rails.root.to_s + '/config/redis.yml')[Rails.env])
 DELIVERY_STATE = {packaging: 'Packaging', ready_to_pick: 'Ready To Pick', confirmed_pickup: 'Confirmed Pickup', out_for_delivery: 'Out For Delivery', delivered: 'Delivered', canceled: 'Canceled'}
