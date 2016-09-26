b6c4d9116a1a844ccecc1e5018efeb99ec26197f
w3villa <gopal.sharma@w3villa.com>
2016-09-26 11:53:02 +0530

    fixed mailer issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'prashant.mishra@w3villa.com',
-    password:             'karbonn@13',
+    user_name:            'admin@pyklocal.com',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
