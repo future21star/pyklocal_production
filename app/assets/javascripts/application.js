c6209fe41f7e7977ded0a0dd9578609d0d03d3f2
w3villa <gopal.sharma@w3villa.com>
2016-09-28 17:30:22 +0530

    fixed issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,7 +47,7 @@ var pyklocal = {
     jQuery('.numbersOnly').keyup(function () { 
       this.value = this.value.replace(/[^0-9\.]/g,'');
     });
-  }
+  },
 
 	showNoty: function() {
 		$.noty.defaults = {
