731d1693b2e2c4e531c93a629a67be465b58c2e6
w3villa <gopal.sharma@w3villa.com>
2016-10-27 13:54:01 +0530

    fixed sell with us

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -3,6 +3,9 @@ Spree::StoreController.class_eval do
   before_action :check_user
 
   def check_user
+    if current_spree_user && (current_spree_user.has_spree_role?("merchant") || current_spree_user.registration_type == "vendor")
+      redirect_to main_app.merchant_stores_path, notice: "You are not a user, need to signup with user role."
+    end
   end
 
 end
