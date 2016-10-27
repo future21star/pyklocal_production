53b59aa095b1ca96c9a92c577d4a8d09c306e755
w3villa <gopal.sharma@w3villa.com>
2016-10-27 14:30:41 +0530

    fixed root_path direction

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -3,8 +3,10 @@ Spree::StoreController.class_eval do
   before_action :check_user
 
   def check_user
-    if current_spree_user && (current_spree_user.has_spree_role?("merchant") || current_spree_user.registration_type == "vendor")
-      redirect_to main_app.merchant_stores_path, notice: "You are not a user, need to signup with user role."
+    unless params[:controller] == "spree/users" && (params[:action] == "edit" || params[:action] == "update")
+      if current_spree_user && (current_spree_user.has_spree_role?("merchant") || current_spree_user.registration_type == "vendor")
+        redirect_to main_app.merchant_stores_path
+      end
     end
   end
 
