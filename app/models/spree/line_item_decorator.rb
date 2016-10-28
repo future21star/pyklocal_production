f710b8a27ae4c255d77d897c558c1b7c8f45ff45
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-10-28 19:21:23 +0530

    Removed commented code

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -14,10 +14,6 @@ module Spree
 			product.images.first.try(:attachment).try(:url)
 		end
 
-		# def store_location
-		# 	{lat: product.try(:store).try(:latitude).to_f, lng: product.try(:store).try(:longitude).to_f}
-		# end
-
 		def seller_name
 			product.store.try(:name)
 		end
@@ -29,22 +25,6 @@ module Spree
 		def pickup_address
 			product.store.try(:address)
 		end
-
-		# def delivery_address
-		# 	[order.ship_address.try(:address1), order.ship_address.try(:address2), order.ship_address.try(:city), order.ship_address.try(:state), order.ship_address.try(:country)].compact.join(", ")
-		# end
-
-		# def buyer_name
-		# 	[order.ship_address.first_name, order.ship_address.last_name].compact.join(" ")
-		# end
-
-		# def buyer_zipcode
-		# 	order.ship_address.zipcode
-		# end
-
-		# def store_zipcode
-		# 	product.store.try(:zipcode)
-		# end
 
 		def copy_tax_category
       if variant
