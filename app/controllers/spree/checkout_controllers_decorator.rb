41b556156fe13e1a0e95bbdbbe6d3222c23b6929
Manish rai <manish.rai@w3villa.com>
2016-11-29 16:29:51 +0530

    fixed new vendor form , fixed address populate method in address state of checkout

--- app/controllers/spree/checkout_controllers_decorator.rb
+++ app/controllers/spree/checkout_controllers_decorator.rb
@@ -3,20 +3,17 @@ Spree::CheckoutController.class_eval do
   before_action :before_address
 
 	def before_address
-  	if current_spree_user.try(:address).blank?
+    if current_spree_user.try(:address).blank?
       if @order.try(:ship_address).present? && @order.try(:bill_address).present?
-        p "(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((999"
-        p @order
-        p @order.try(:ship_address).present?
-        p  @order.try(:bill_address).present?
         @order.ship_address = @order.ship_address 
         @order.bill_address = @order.bill_address 
+      elsif current_spree_user.orders.where("state = ? OR state = ?", "complete", "canceled").present?
+        @order.ship_address = current_spree_user.orders.where("state = ? OR state = ?", "complete", "canceled").last.bill_address
+        @order.bill_address = current_spree_user.orders.where("state = ? OR state =?", "complete", "canceled").last.ship_address
       elsif current_spree_user.try(:ship_address).present? && current_spree_user.try(:bill_address).present?
-        p "0000000000000000000000000000000000000000000000000000000000000000000000000"
-        @order.ship_address =  current_spree_user.bill_address
-        @order.bill_address =  current_spree_user.ship_address
+        @order.ship_address =  current_spree_user.ship_address
+        @order.bill_address =  current_spree_user.bill_address
       else
-        p "*********************************************************************************88"
     		@order.ship_address = Spree::Address.build_default
     		@order.bill_address = Spree::Address.build_default
       end
@@ -24,7 +21,6 @@ Spree::CheckoutController.class_eval do
         @order.attributes = {is_existing: params[:is_existing]}
       end
   	else
-      p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4444"
   	  @order.ship_address = current_spree_user.address
   	  @order.bill_address = current_spree_user.address
       if params[:is_existing]
