36dff1afb8fbda8701abf8f8d11c12a2791b8bb5
manish rai <manish.rai@w3villa.com>
2017-04-14 16:43:21 +0530

    fix duplicate email error

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -8,35 +8,39 @@ module Spree
     end
     
     def create
-      @tab = params[:tab]
-      @user = build_resource(spree_user_params)
-      resource_saved = resource.save
-      yield resource if block_given?
-      if resource_saved
-        if resource.active_for_authentication?
-          set_flash_message :notice, spree_user_params["stores_attributes"].present? ? :inactive_store : :signed_up
-          sign_up(resource_name, resource)
-          session[:spree_user_signup] = true
-          if @user.has_store
-            respond_with @user.stores.first.slug, :location => after_sign_up_path_for_merchant(@user.stores.first.slug)
-          elsif @user.registration_type == "vendor"
-            respond_with @user , :location => after_sign_up_path
+      unless Spree::User.with_deleted.find_by_email(params[:spree_user][:email]).present?
+        @tab = params[:tab]
+        @user = build_resource(spree_user_params)
+        resource_saved = resource.save
+        yield resource if block_given?
+        if resource_saved
+          if resource.active_for_authentication?
+            set_flash_message :notice, spree_user_params["stores_attributes"].present? ? :inactive_store : :signed_up
+            sign_up(resource_name, resource)
+            session[:spree_user_signup] = true
+            if @user.has_store
+              respond_with @user.stores.first.slug, :location => after_sign_up_path_for_merchant(@user.stores.first.slug)
+            elsif @user.registration_type == "vendor"
+              respond_with @user , :location => after_sign_up_path
+            else
+              respond_with resource, location: after_sign_up_path_for(resource)
+            end
           else
-            respond_with resource, location: after_sign_up_path_for(resource)
+            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
           end
         else
-          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          if spree_user_params["stores_attributes"].present?
+            flash[:error] = resource.errors.full_messages.join(", ")
+            redirect_to new_store_application_url
+          else
+            flash.now[:error] = resource.errors.full_messages.join(", ")
+            render action: :new
+          end
         end
       else
-        if spree_user_params["stores_attributes"].present?
-          flash[:error] = resource.errors.full_messages.join(", ")
-          redirect_to new_store_application_url
-        else
-          flash.now[:error] = resource.errors.full_messages.join(", ")
-          render action: :new
-        end
+        redirect_to :back, notice: "Email is already registered"
       end
     end
 
