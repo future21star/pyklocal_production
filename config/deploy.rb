4e719c50642f5af1e00714adec5b4323ebb291ce
awhaskell <awhaskell>
2017-05-17 10:40:33 -0400

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,161 +1,134 @@
-lock '3.4.0'
+require 'bundler/capistrano'
+require 'rvm/capistrano'
+# require 'whenever/capistrano'
+require 'capistrano/ext/multistage'
+# require 'capistrano-unicorn'
 
-set :application, 'contactbook'
-set :repo_url, 'git@github.com:/pyklocal/pyklocal.git' # Edit this to match your repository
-set :branch, "vendor_dashboard#2"
-set :deploy_to, '/home/pyklocal/pyklocal'
-set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.4.0' # Edit this if you are using MRI Ruby
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
-# require 'bundler/capistrano'
-# require 'rvm/capistrano'
-# # require 'whenever/capistrano'
-# require 'capistrano/ext/multistage'
-# # require 'capistrano-unicorn'
-
-# # Whenever setup for application
-# set(:whenever_command) { "RAILS_ENV=#{rails_env} bundle exec whenever" }
-# set :whenever_environment, defer { 'production' }
+# Whenever setup for application
+set(:whenever_command) { "RAILS_ENV=#{rails_env} bundle exec whenever" }
+set :whenever_environment, defer { 'production' }
 
 
-# # Application configuration
-# set :application, 'pyklocal'
-# set :repository,  'git@github.com:/pyklocal/pyklocal.git'
-# set :branch, 'vendor_dashboard#2'
-# set :scm, :git
+# Application configuration
+set :application, 'pyklocal'
+set :repository,  'git@github.com:/pyklocal/pyklocal.git'
+set :branch, 'master'
+set :scm, :git
 
-# # Server-side system wide settings
-# default_run_options[:pty] = true
-# ssh_options[:forward_agent] = true
+# Server-side system wide settings
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# # Application stages configuration
-# set :stages, %w(production-frontend production-production production)
-# set :default_stage, 'production'
+# Application stages configuration
+set :stages, %w(production-frontend production-production production)
+set :default_stage, 'production'
 
 
-# # Unicorn environment configuration
-# set(:unicorn_env) { rails_env }
+# Unicorn environment configuration
+set(:unicorn_env) { rails_env }
 
-# # Deploy configuration (Unicorn, nginx)
-# after 'deploy', 'deploy:cleanup'
+# Deploy configuration (Unicorn, nginx)
+after 'deploy', 'deploy:cleanup'
 
-# namespace :deploy do
+namespace :deploy do
 
-#   task :setup_config, roles: :app do
-#     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-#     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-#     run "mkdir -p #{shared_path}/config"
-#     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-#     puts "Now edit the config files in #{shared_path}."
-#   end
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
 
-#   after "deploy:setup", "deploy:setup_config"
+  after "deploy:setup", "deploy:setup_config"
 
-#   task :symlink_config, roles: :app do
-#     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-#     run "ln -nfs #{shared_path}/public/spree #{release_path}/public/spree"
-#   end
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/public/spree #{release_path}/public/spree"
+  end
 
-#   after "deploy:finalize_update", "deploy:symlink_config"
+  after "deploy:finalize_update", "deploy:symlink_config"
 
-#   desc "Make sure local git is in sync with remote."
-#     task :check_revision, roles: :web do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#       puts "WARNING: HEAD is not the same as origin/master"
-#       puts "Run `git push` to sync changes."
-#       exit
-#     end
-#   end
+  desc "Make sure local git is in sync with remote."
+    task :check_revision, roles: :web do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
 
-#   # before "deploy", "deploy:check_revision"
+  # before "deploy", "deploy:check_revision"
 
-#   namespace :figaro do
-#     desc "SCP transfer figaro configuration to the shared folder"
-#     task :setup do
-#       transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
-#     end
+  namespace :figaro do
+    desc "SCP transfer figaro configuration to the shared folder"
+    task :setup do
+      transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
+    end
 
-#     desc "Symlink application.yml to the release path"
-#     task :symlink do
-#       run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
-#     end
-#   end
+    desc "Symlink application.yml to the release path"
+    task :symlink do
+      run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
+    end
+  end
 
-#   after  "deploy:started", "figaro:setup"
-#   after "deploy:symlink:release", "figaro:symlink"
-#   desc "Restart the application"
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{current_path}/tmp/pids/puma-production.state restart"
-#   end
+  after  "deploy:started", "figaro:setup"
+  after "deploy:symlink:release", "figaro:symlink"
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{current_path}/tmp/pids/puma-production.state restart"
+  end
 
-#   # desc <<-DESC
-#   #   Clean up any assets that haven't been deployed for more than :expire_assets_after seconds.
-#   #   Default time to keep old assets is one week. Set the :expire_assets_after variable
-#   #   to change the assets expiry time. Assets will only be deleted if they are not required by
-#   #   an existing release.
-#   # DESC
+  desc <<-DESC
+    Clean up any assets that haven't been deployed for more than :expire_assets_after seconds.
+    Default time to keep old assets is one week. Set the :expire_assets_after variable
+    to change the assets expiry time. Assets will only be deleted if they are not required by
+    an existing release.
+  DESC
   
-#   # task :clean_expired, :roles => lambda { assets_role }, :except => { :no_release => true } do
-#   #   # Fetch all assets_manifest contents.
-#   #   manifests_output = capture <<-CMD.compact
-#   #     for manifest in #{releases_path.shellescape}/*/assets_manifest.*; do
-#   #       cat -- "$manifest" 2> /dev/null && printf ':::' || true;
-#   #     done
-#   #   CMD
-#   #   manifests = manifests_output.split(':::')
+  task :clean_expired, :roles => lambda { assets_role }, :except => { :no_release => true } do
+    # Fetch all assets_manifest contents.
+    manifests_output = capture <<-CMD.compact
+      for manifest in #{releases_path.shellescape}/*/assets_manifest.*; do
+        cat -- "$manifest" 2> /dev/null && printf ':::' || true;
+      done
+    CMD
+    manifests = manifests_output.split(':::')
 
-#   #   if manifests.empty?
-#   #     logger.info "No manifests in #{releases_path}/*/assets_manifest.*"
-#   #   else
-#   #     logger.info "Fetched #{manifests.count} manifests from #{releases_path}/*/assets_manifest.*"
-#   #     current_assets = Set.new
-#   #     manifests.each do |content|
-#   #       current_assets += parse_manifest(content)
-#   #     end
-#   #     current_assets += [File.basename(shared_manifest_path), "sources_manifest.yml"]
+    if manifests.empty?
+      logger.info "No manifests in #{releases_path}/*/assets_manifest.*"
+    else
+      logger.info "Fetched #{manifests.count} manifests from #{releases_path}/*/assets_manifest.*"
+      current_assets = Set.new
+      manifests.each do |content|
+        current_assets += parse_manifest(content)
+      end
+      current_assets += [File.basename(shared_manifest_path), "sources_manifest.yml"]
 
-#   #     # Write the list of required assets to server.
-#   #     logger.info "Writing required assets to #{deploy_to}/REQUIRED_ASSETS..."
-#   #     escaped_assets = current_assets.sort.join("\n").gsub("\"", "\\\"") << "\n"
-#   #     put escaped_assets, "#{deploy_to}/REQUIRED_ASSETS", :via => :scp
+      # Write the list of required assets to server.
+      logger.info "Writing required assets to #{deploy_to}/REQUIRED_ASSETS..."
+      escaped_assets = current_assets.sort.join("\n").gsub("\"", "\\\"") << "\n"
+      put escaped_assets, "#{deploy_to}/REQUIRED_ASSETS", :via => :scp
 
-#   #     # Finds all files older than X minutes, then removes them if they are not referenced
-#   #     # in REQUIRED_ASSETS.
-#   #     expire_after_mins = (expire_assets_after.to_f / 60.0).to_i
-#   #     logger.info "Removing assets that haven't been deployed for #{expire_after_mins} minutes..."
-#   #     # LC_COLLATE=C tells the `sort` and `comm` commands to sort files in byte order.
-#   #     run <<-CMD.compact
-#   #       cd -- #{deploy_to.shellescape}/ &&
-#   #       LC_COLLATE=C sort REQUIRED_ASSETS -o REQUIRED_ASSETS &&
-#   #       cd -- #{shared_path.shellescape}/#{shared_assets_prefix}/ &&
-#   #       for f in $(
-#   #         find * -mmin +#{expire_after_mins.to_s.shellescape} -type f | LC_COLLATE=C sort |
-#   #         LC_COLLATE=C comm -23 -- - #{deploy_to.shellescape}/REQUIRED_ASSETS
-#   #       ); do
-#   #         echo "Removing unneeded asset: $f";
-#   #         rm -f -- "$f";
-#   #       done;
-#   #       rm -f -- #{deploy_to.shellescape}/REQUIRED_ASSETS
-#   #     CMD
-#   #   end
-#   # end
+      # Finds all files older than X minutes, then removes them if they are not referenced
+      # in REQUIRED_ASSETS.
+      expire_after_mins = (expire_assets_after.to_f / 60.0).to_i
+      logger.info "Removing assets that haven't been deployed for #{expire_after_mins} minutes..."
+      # LC_COLLATE=C tells the `sort` and `comm` commands to sort files in byte order.
+      run <<-CMD.compact
+        cd -- #{deploy_to.shellescape}/ &&
+        LC_COLLATE=C sort REQUIRED_ASSETS -o REQUIRED_ASSETS &&
+        cd -- #{shared_path.shellescape}/#{shared_assets_prefix}/ &&
+        for f in $(
+          find * -mmin +#{expire_after_mins.to_s.shellescape} -type f | LC_COLLATE=C sort |
+          LC_COLLATE=C comm -23 -- - #{deploy_to.shellescape}/REQUIRED_ASSETS
+        ); do
+          echo "Removing unneeded asset: $f";
+          rm -f -- "$f";
+        done;
+        rm -f -- #{deploy_to.shellescape}/REQUIRED_ASSETS
+      CMD
+    end
+  end
 
-# end
+end
