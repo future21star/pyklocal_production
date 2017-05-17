adc43d4c7205085880afd43cd997c3ddc27fd32d
awhaskell <awhaskell>
2017-05-17 08:36:16 -0400

    update deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,56 +79,56 @@ namespace :deploy do
     run "cd #{current_path} && RAILS_ENV=#{stage} bundle exec pumactl -S #{current_path}/tmp/pids/puma-production.state restart"
   end
 
-  desc <<-DESC
-    Clean up any assets that haven't been deployed for more than :expire_assets_after seconds.
-    Default time to keep old assets is one week. Set the :expire_assets_after variable
-    to change the assets expiry time. Assets will only be deleted if they are not required by
-    an existing release.
-  DESC
+  # desc <<-DESC
+  #   Clean up any assets that haven't been deployed for more than :expire_assets_after seconds.
+  #   Default time to keep old assets is one week. Set the :expire_assets_after variable
+  #   to change the assets expiry time. Assets will only be deleted if they are not required by
+  #   an existing release.
+  # DESC
   
-  task :clean_expired, :roles => lambda { assets_role }, :except => { :no_release => true } do
-    # Fetch all assets_manifest contents.
-    manifests_output = capture <<-CMD.compact
-      for manifest in #{releases_path.shellescape}/*/assets_manifest.*; do
-        cat -- "$manifest" 2> /dev/null && printf ':::' || true;
-      done
-    CMD
-    manifests = manifests_output.split(':::')
+  # task :clean_expired, :roles => lambda { assets_role }, :except => { :no_release => true } do
+  #   # Fetch all assets_manifest contents.
+  #   manifests_output = capture <<-CMD.compact
+  #     for manifest in #{releases_path.shellescape}/*/assets_manifest.*; do
+  #       cat -- "$manifest" 2> /dev/null && printf ':::' || true;
+  #     done
+  #   CMD
+  #   manifests = manifests_output.split(':::')
 
-    if manifests.empty?
-      logger.info "No manifests in #{releases_path}/*/assets_manifest.*"
-    else
-      logger.info "Fetched #{manifests.count} manifests from #{releases_path}/*/assets_manifest.*"
-      current_assets = Set.new
-      manifests.each do |content|
-        current_assets += parse_manifest(content)
-      end
-      current_assets += [File.basename(shared_manifest_path), "sources_manifest.yml"]
+  #   if manifests.empty?
+  #     logger.info "No manifests in #{releases_path}/*/assets_manifest.*"
+  #   else
+  #     logger.info "Fetched #{manifests.count} manifests from #{releases_path}/*/assets_manifest.*"
+  #     current_assets = Set.new
+  #     manifests.each do |content|
+  #       current_assets += parse_manifest(content)
+  #     end
+  #     current_assets += [File.basename(shared_manifest_path), "sources_manifest.yml"]
 
-      # Write the list of required assets to server.
-      logger.info "Writing required assets to #{deploy_to}/REQUIRED_ASSETS..."
-      escaped_assets = current_assets.sort.join("\n").gsub("\"", "\\\"") << "\n"
-      put escaped_assets, "#{deploy_to}/REQUIRED_ASSETS", :via => :scp
+  #     # Write the list of required assets to server.
+  #     logger.info "Writing required assets to #{deploy_to}/REQUIRED_ASSETS..."
+  #     escaped_assets = current_assets.sort.join("\n").gsub("\"", "\\\"") << "\n"
+  #     put escaped_assets, "#{deploy_to}/REQUIRED_ASSETS", :via => :scp
 
-      # Finds all files older than X minutes, then removes them if they are not referenced
-      # in REQUIRED_ASSETS.
-      expire_after_mins = (expire_assets_after.to_f / 60.0).to_i
-      logger.info "Removing assets that haven't been deployed for #{expire_after_mins} minutes..."
-      # LC_COLLATE=C tells the `sort` and `comm` commands to sort files in byte order.
-      run <<-CMD.compact
-        cd -- #{deploy_to.shellescape}/ &&
-        LC_COLLATE=C sort REQUIRED_ASSETS -o REQUIRED_ASSETS &&
-        cd -- #{shared_path.shellescape}/#{shared_assets_prefix}/ &&
-        for f in $(
-          find * -mmin +#{expire_after_mins.to_s.shellescape} -type f | LC_COLLATE=C sort |
-          LC_COLLATE=C comm -23 -- - #{deploy_to.shellescape}/REQUIRED_ASSETS
-        ); do
-          echo "Removing unneeded asset: $f";
-          rm -f -- "$f";
-        done;
-        rm -f -- #{deploy_to.shellescape}/REQUIRED_ASSETS
-      CMD
-    end
-  end
+  #     # Finds all files older than X minutes, then removes them if they are not referenced
+  #     # in REQUIRED_ASSETS.
+  #     expire_after_mins = (expire_assets_after.to_f / 60.0).to_i
+  #     logger.info "Removing assets that haven't been deployed for #{expire_after_mins} minutes..."
+  #     # LC_COLLATE=C tells the `sort` and `comm` commands to sort files in byte order.
+  #     run <<-CMD.compact
+  #       cd -- #{deploy_to.shellescape}/ &&
+  #       LC_COLLATE=C sort REQUIRED_ASSETS -o REQUIRED_ASSETS &&
+  #       cd -- #{shared_path.shellescape}/#{shared_assets_prefix}/ &&
+  #       for f in $(
+  #         find * -mmin +#{expire_after_mins.to_s.shellescape} -type f | LC_COLLATE=C sort |
+  #         LC_COLLATE=C comm -23 -- - #{deploy_to.shellescape}/REQUIRED_ASSETS
+  #       ); do
+  #         echo "Removing unneeded asset: $f";
+  #         rm -f -- "$f";
+  #       done;
+  #       rm -f -- #{deploy_to.shellescape}/REQUIRED_ASSETS
+  #     CMD
+  #   end
+  # end
 
 end
