ae08dcb66d9c4dcb36ac343b084fa739319cac97
awhaskell <awhaskell>
2017-05-17 10:36:35 -0400

    update deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,61 @@
-require 'bundler/capistrano'
-require 'rvm/capistrano'
-# require 'whenever/capistrano'
-require 'capistrano/ext/multistage'
-# require 'capistrano-unicorn'
+lock '3.4.0'
 
-# Whenever setup for application
-set(:whenever_command) { "RAILS_ENV=#{rails_env} bundle exec whenever" }
-set :whenever_environment, defer { 'production' }
+set :application, 'contactbook'
+set :repo_url, 'git@github.com:/pyklocal/pyklocal.git' # Edit this to match your repository
+set :branch, "vendor_dashboard#2"
+set :deploy_to, '/home/pyklocal/pyklocal'
+set :pty, true
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.4.0' # Edit this if you are using MRI Ruby
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
+# require 'bundler/capistrano'
+# require 'rvm/capistrano'
+# # require 'whenever/capistrano'
+# require 'capistrano/ext/multistage'
+# # require 'capistrano-unicorn'
+
+# # Whenever setup for application
+# set(:whenever_command) { "RAILS_ENV=#{rails_env} bundle exec whenever" }
+# set :whenever_environment, defer { 'production' }
 
 
-# Application configuration
-set :application, 'pyklocal'
-set :repository,  'git@github.com:/pyklocal/pyklocal.git'
-set :branch, 'vendor_dashboard#2'
-set :scm, :git
+# # Application configuration
+# set :application, 'pyklocal'
+# set :repository,  'git@github.com:/pyklocal/pyklocal.git'
+# set :branch, 'vendor_dashboard#2'
+# set :scm, :git
 
-# Server-side system wide settings
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+# # Server-side system wide settings
+# default_run_options[:pty] = true
+# ssh_options[:forward_agent] = true
 
-# Application stages configuration
-set :stages, %w(production-frontend production-production production)
-set :default_stage, 'production'
+# # Application stages configuration
+# set :stages, %w(production-frontend production-production production)
+# set :default_stage, 'production'
 
 
-# Unicorn environment configuration
-set(:unicorn_env) { rails_env }
+# # Unicorn environment configuration
+# set(:unicorn_env) { rails_env }
 
-# Deploy configuration (Unicorn, nginx)
-after 'deploy', 'deploy:cleanup'
+# # Deploy configuration (Unicorn, nginx)
+# after 'deploy', 'deploy:cleanup'
 
 # namespace :deploy do
 
