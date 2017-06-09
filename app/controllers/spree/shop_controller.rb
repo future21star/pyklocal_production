9d50c1a12510b171eab0e8f7479e8e2f6f6b9895
awhaskell <awhaskell>
2017-06-09 02:36:47 -0400

    update sunspot for search

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -28,18 +28,20 @@ class Spree::ShopController < Spree::StoreController
   private 
 
     def load_all_facets
+      # min_price = Spree::Product.min_price
+      # max_price = Spree::Product.max_price
       @all_facets = Sunspot.search(Spree::Product) do 
         fulltext "*#{params[:q][:search]}*"  if params[:q] && params[:q][:search]
-        # if params[:q] && params[:q][:category]
-        #   with(:taxon_name, params[:q][:category])
-        # end
+        if params[:q] && params[:q][:category]
+          with(:taxon_name, params[:q][:category])
+        end
         if params[:q] && params[:q][:categories]
           any_of do 
             params[:q][:categories].each do |category|
               with(:taxon_name, category)
             end
           end
-        end        
+        end
         if params[:q] && params[:q][:brand]
           any_of do 
             params[:q][:brand].each do |brand|
@@ -65,7 +67,7 @@ class Spree::ShopController < Spree::StoreController
         with(:visible, :true)
         with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
         with(:taxon_ids, Spree::Taxon.where(permalink: params[:id]).collect(&:id)) if params[:id].present?
-        facet(:price, :range => Spree::Product.min_price..Spree::Product.max_price, :range_interval => 100)
+        facet(:price, :range => 0..100000, :range_interval => 100)
         facet(:brand_name)
         facet(:store_name)
         facet(:taxon_name)
@@ -82,6 +84,19 @@ class Spree::ShopController < Spree::StoreController
       per_page = params[:q] && params[:q][:per_page] ? params[:q][:per_page] : 12
       @search = Sunspot.search(Spree::Product) do 
         fulltext "*#{params[:q][:search]}*" if params[:q] && params[:q][:search]
+        paginate(:page => params[:page], :per_page => per_page)
+        if params[:q] && params[:q][:category] && params[:q][:category] != "all"
+          with(:taxon_name, params[:q][:category])
+        end
+        with(:buyable, :true)
+        with(:visible, :true)
+        with(:taxon_ids, Spree::Taxon.where(permalink: params[:id]).collect(&:id)) if params[:id].present?
+        with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
+        facet(:price, :range => 0..100000, :range_interval => 100)
+        facet(:brand_name)
+        facet(:store_name)
+        facet(:taxon_name)
+
         if params[:q] && params[:q][:categories]
           any_of do 
             params[:q][:categories].each do |category|
@@ -112,26 +127,12 @@ class Spree::ShopController < Spree::StoreController
             end
           end
         end
-        with(:buyable, :true)
-        with(:visible, :true)
-        with(:taxon_ids, Spree::Taxon.where(permalink: params[:id]).collect(&:id)) if params[:id].present?
-        with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
-        facet(:price, :range => Spree::Product.min_price..Spree::Product.max_price, :range_interval => 100)
-        facet(:brand_name)
-        facet(:store_name)
-        facet(:taxon_name)
-
         if (params[:q] && params[:q][:sort_by]) && (params[:q][:sort_by] == "Highest Price")
           order_by(:price, :desc)
         end
         if (params[:q] && params[:q][:sort_by]) && (params[:q][:sort_by] == "Lowest Price")
           order_by(:price, :asc) if params[:q] && params[:q][:sort_by]
-        end        
-        paginate(:page => params[:page], :per_page => per_page)
-        # if params[:q] && params[:q][:category] && params[:q][:category] != "all"
-        #   with(:taxon_name, params[:q][:category])
-        # end
-
+        end
       end
     end
 
