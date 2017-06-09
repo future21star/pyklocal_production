b7c8f80c03213463987339a910355497b059e803
awhaskell <awhaskell>
2017-06-09 00:28:58 -0400

    update conf for sunspot

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -30,16 +30,16 @@ class Spree::ShopController < Spree::StoreController
     def load_all_facets
       @all_facets = Sunspot.search(Spree::Product) do 
         fulltext "*#{params[:q][:search]}*"  if params[:q] && params[:q][:search]
-        if params[:q] && params[:q][:category]
-          with(:taxon_name, params[:q][:category])
-        end
+        # if params[:q] && params[:q][:category]
+        #   with(:taxon_name, params[:q][:category])
+        # end
         if params[:q] && params[:q][:categories]
           any_of do 
             params[:q][:categories].each do |category|
               with(:taxon_name, category)
             end
           end
-        end
+        end        
         if params[:q] && params[:q][:brand]
           any_of do 
             params[:q][:brand].each do |brand|
@@ -82,19 +82,6 @@ class Spree::ShopController < Spree::StoreController
       per_page = params[:q] && params[:q][:per_page] ? params[:q][:per_page] : 12
       @search = Sunspot.search(Spree::Product) do 
         fulltext "*#{params[:q][:search]}*" if params[:q] && params[:q][:search]
-        paginate(:page => params[:page], :per_page => per_page)
-        if params[:q] && params[:q][:category] && params[:q][:category] != "all"
-          with(:taxon_name, params[:q][:category])
-        end
-        with(:buyable, :true)
-        with(:visible, :true)
-        with(:taxon_ids, Spree::Taxon.where(permalink: params[:id]).collect(&:id)) if params[:id].present?
-        with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
-        facet(:price, :range => Spree::Product.min_price..Spree::Product.max_price, :range_interval => 100)
-        facet(:brand_name)
-        facet(:store_name)
-        facet(:taxon_name)
-
         if params[:q] && params[:q][:categories]
           any_of do 
             params[:q][:categories].each do |category|
@@ -125,12 +112,26 @@ class Spree::ShopController < Spree::StoreController
             end
           end
         end
+        with(:buyable, :true)
+        with(:visible, :true)
+        with(:taxon_ids, Spree::Taxon.where(permalink: params[:id]).collect(&:id)) if params[:id].present?
+        with(:location).in_radius(params[:q][:lat], params[:q][:lng], params[:q][:radius].to_i, bbox: true) if params[:q] && params[:q][:lat].present? && params[:q][:lng].present?
+        facet(:price, :range => Spree::Product.min_price..Spree::Product.max_price, :range_interval => 100)
+        facet(:brand_name)
+        facet(:store_name)
+        facet(:taxon_name)
+
         if (params[:q] && params[:q][:sort_by]) && (params[:q][:sort_by] == "Highest Price")
           order_by(:price, :desc)
         end
         if (params[:q] && params[:q][:sort_by]) && (params[:q][:sort_by] == "Lowest Price")
           order_by(:price, :asc) if params[:q] && params[:q][:sort_by]
-        end
+        end        
+        paginate(:page => params[:page], :per_page => per_page)
+        # if params[:q] && params[:q][:category] && params[:q][:category] != "all"
+        #   with(:taxon_name, params[:q][:category])
+        # end
+
       end
     end
 
