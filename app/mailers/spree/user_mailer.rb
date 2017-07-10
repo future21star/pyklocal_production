2b1cc5f1071754b660a98ac5933d51bc2db6328e
awhaskell <awhaskell>
2017-07-10 03:21:31 -0400

    update  confirmation email for new server

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -14,7 +14,7 @@ module Spree
     end
 
     def confirmation_instructions(user, token, opts={})
-      @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Store.current.url)
+      @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Rails.application.secrets.HOST_URL)
       mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])
     end
 
