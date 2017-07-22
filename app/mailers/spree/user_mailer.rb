39c69e8e5ed8c0b406dde35fdc6e3cf31d72a637
awhaskell <awhaskell>
2017-07-22 07:51:31 -0400

    fix order line items & reset password confirmation

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -8,7 +8,7 @@ module Spree
     end
 
     def reset_password_instructions(user, token, *args)
-      @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Store.current.url)
+      @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Rails.application.secrets.HOST_URL)
 
       mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
     end
